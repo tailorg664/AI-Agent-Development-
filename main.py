4de694dbd60fb5cb1261984c30756c6a9f92abf9
Gaurav Tailor <tailorg664@gmail.com>
2026-06-26 09:04:31 +0000

    Class Best Practice

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-# dict declaration : {}
-my_dict = {}
-my_dict = {"name": "John", "age": 30, "city": "New York"}
-print(my_dict)
-for key in my_dict:
-      print(key, my_dict.get(key))
+class Dog:
+      
+      def __init__(self,name):
+            self.name = name
+            self.tricks = []
+      def add_trick(self,trick):
+            self.tricks.append(trick)
+      
 
-# assignment of variables;
-a,b,c = 5, 10, 15
-print(a,b,c)
-a,b,c = b,c,a
-print(a,b,c)
-scores = [90, 80, 70, 60,40,90,70]
-unique_scores = set(scores)
-print(unique_scores)
+############################
+
+german = Dog("German Shepherd")
+german.add_trick("Sit")
+labrador = Dog("Labrador")
+labrador.add_trick("Fetch")
+print(german.tricks)
+print(labrador.tricks)
