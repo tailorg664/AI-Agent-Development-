7cc0a427c40bbd876d4d979e52f4a1723c81b1c4
Gaurav Tailor <tailorg664@gmail.com>
2026-06-26 08:14:11 +0000

    added basic.txt and modified main.py and python.txt

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-import requests
-request = requests.get("https://api.github.com")
-print(request.status_code)
+# dict declaration : {}
+my_dict = {}
+my_dict = {"name": "John", "age": 30, "city": "New York"}
+print(my_dict)
+for key in my_dict:
+      print(key, my_dict.get(key))
+
+# assignment of variables;
+a,b,c = 5, 10, 15
+print(a,b,c)
+a,b,c = b,c,a
+print(a,b,c)
+scores = [90, 80, 70, 60,40,90,70]
+unique_scores = set(scores)
+print(unique_scores)
